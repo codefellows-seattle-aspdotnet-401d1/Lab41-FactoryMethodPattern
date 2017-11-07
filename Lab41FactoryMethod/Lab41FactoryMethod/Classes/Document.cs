9cfdd1393d24a82e502747206876ba76518283b8
Gregory Dukes <kcils360@gmail.com>
2017-11-07 12:01:40 -0800

    initial buildout

---
+++ Lab41FactoryMethod/Lab41FactoryMethod/Classes/Document.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab41FactoryMethod.Classes
+{
+    public class Document
+    {
+        public string DocType { get; set; }
+        public string Author { get; set; }
+        public List<string> Pages = new List<string>();
+        public string GetType()
+        {
+            return DocType;
+        }
+
+        public void Body()
+        {
+            string d = GetType();
+            Console.WriteLine($"This {d} Document contains...");
+            foreach(string pag in Pages)
+            {
+                Console.WriteLine(pag);
+            }
+        }
+    }
+}
