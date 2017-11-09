196cbabbb5ee2d145d7578bbf597b1e1d74ef287
Gregory Dukes <kcils360@gmail.com>
2017-11-09 10:22:14 -0800

    mostly built

---
+++ Lab41FactoryMethod/Lab41FactoryMethod/Classes/Factorys/DocMakerFactory.cs
@@ -0,0 +1,39 @@
+﻿using Lab41FactoryMethod.Classes.Documents;
+using Lab41FactoryMethod.Classes.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab41FactoryMethod.Classes.Factorys
+{
+    class DocMakerFactory
+    {
+        public Document CreateDocument(string docType)
+        {
+            Document document = new Document();
+            switch(docType)
+            {
+                case "Resume":
+                    document = new Resume();
+                    Console.WriteLine("Make a resume");
+                    break;
+
+                case "Report":
+                    document = new Report();
+                    Console.WriteLine("Make a report");
+                    break;
+
+                case "License":
+                    document = new License();
+                    Console.WriteLine("Make a License");
+                    break;
+
+                case "Will":
+                    document = new Will();
+                    Console.WriteLine("Make a Will");
+                    break;
+            }
+            return document;
+        }
+    }
+}
