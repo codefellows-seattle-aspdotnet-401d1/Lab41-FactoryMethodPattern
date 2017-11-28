d7a916e7260f2a7839284856e5770bf8e4e951c8
Brandon Little <brlittle86@gmail.com>
2017-11-27 16:07:14 -0800

    initial solution creation

---
+++ ResumeFactory/ResumeFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ResumeFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
