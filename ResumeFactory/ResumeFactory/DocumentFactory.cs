92878bc7b6ab672536cbe0256bee54c9f7efc064
Brandon Little <brlittle86@gmail.com>
2017-11-27 16:34:12 -0800

    created DocumentFactory method to create documents

--- ResumeFactory/ResumeFactory/DocumentFactory.cs
+++ ResumeFactory/ResumeFactory/DocumentFactory.cs
@@ -6,5 +6,32 @@ namespace ResumeFactory
 {
     class DocumentFactory
     {
+        public Document CreateDocument(string type)
+        {
+            switch (type)
+            {
+                case "resume":
+                    Document resume = new Resume();
+                    resume.AddPage(new Page("Cover Letter"));
+                    resume.AddPage(new Page("Personal Statement"));
+                    resume.AddPage(new Page("Skills"));
+                    resume.AddPage(new Page("Experience"));
+                    resume.AddPage(new Page("Education"));
+                    return resume;
+                case "report":
+                    Document report = new Report();
+                    report.AddPage(new Page("Introduction"));
+                    report.AddPage(new Page("Overview"));
+                    report.AddPage(new Page("Statement"));
+                    report.AddPage(new Page("Arguments"));
+                    report.AddPage(new Page("Data"));
+                    report.AddPage(new Page("Conclusion"));
+                    report.AddPage(new Page("Sources and Bibliography"));
+                    return report;
+                default:
+                    Document document = new Document();
+                    return document;
+            }
+        }
     }
 }
