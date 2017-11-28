997eb1292db0ca69de6d8d1d641150a70f2fe278
Brandon Little <brlittle86@gmail.com>
2017-11-27 16:19:17 -0800

    filled out Document methods and properties

--- ResumeFactory/ResumeFactory/Document.cs
+++ ResumeFactory/ResumeFactory/Document.cs
@@ -6,5 +6,24 @@ namespace ResumeFactory
 {
     class Document
     {
+        List<Page> Pages { get; set; }
+
+        public Document()
+        {
+            Pages = new List<Page>();
+        }
+
+        public void AddPage(Page page)
+        {
+            Pages.Add(page);
+        }
+
+        public void PrintPages()
+        {
+            foreach (var page in Pages)
+            {
+                Console.WriteLine(page.Type);
+            }
+        }
     }
 }
